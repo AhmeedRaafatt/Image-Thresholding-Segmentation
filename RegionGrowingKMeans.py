924de5288b88d42c48a501ba1ccae6d564e6e689
Ahmed Raafat <125607744+AhmeedRaafatt@users.noreply.github.com>
2025-04-25 22:37:29 +0300

    Built in done

--- RegionGrowingKMeans.py
+++ RegionGrowingKMeans.py
@@ -29,12 +29,39 @@ class RegionGrowingKMeans:
         try:
             ###
             result_image = image
+            if method == "k-means":
+                result_image = self.kmeans_segmentation(image, k=3)
+            elif method == "region growing":
+                pass
 
+
+            print("image is returned")
+            
             
             ###
             pub.sendMessage(
                 Topics.SEGMENTATION_RESULT,
                 result_image=result_image
                 )
+            
+            return result_image
         except Exception as e:
             logging.error(f"Error in {method} segmentation: {str(e)}")
+
+    def kmeans_segmentation(self, image, k=3):
+        # Reshape the image to a 2D array of pixels
+        pixel_values = image.reshape((-1, 3))
+        pixel_values = np.float32(pixel_values)
+
+        # Define criteria and apply kmeans
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+        _, labels, centers = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+        # Convert back to uint8 and reshape to original image
+        centers = np.uint8(centers)
+        segmented_image = centers[labels.flatten()]
+        segmented_image = segmented_image.reshape(image.shape)
+        print('Segmenting Kmeans Done')
+
+
+        return segmented_image
